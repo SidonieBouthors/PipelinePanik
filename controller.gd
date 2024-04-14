85fa98d2f2385da52bf21e13bf199629703334dc
Patrick Pataky <patrick.pataky@epfl.ch>
2024-04-14 21:25:33 +0200

    for some reasons the timer is not found

--- controller.gd
+++ controller.gd
@@ -1,6 +1,8 @@
 extends Node
 class_name Controller
 
+@onready var timer: Timer = get_node("Timer")
+
 var clock_cycle_counter := 0
 var instructions : Array
 var first_units : Array
@@ -10,9 +12,8 @@ signal increment_clock(clock_cycle_counter)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	reset_counter()
+	print(timer)
 	start_clock()
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -29,32 +30,36 @@ func _on_timer_timeout():
 			unit.instr = instructions.pop_at(0)
 	
 	increment_clock.emit(clock_cycle_counter)
-	last_unit.run()
+	
+	if last_unit:
+		last_unit.run()
 
 func run():
-	reset_counter()
 	start_clock()
 
 func start_clock():
-	$Timer.start()
+	timer.start()
+	pass
 
 func pause_clock():
-	$Timer.stop()
+	timer.stop()
+	pass
 	
 func reset_counter():
 	clock_cycle_counter = 0
-	pause_clock()	
+	pause_clock()
 
 func _print_state():
 	print("Clock cycle: ", clock_cycle_counter)
 
-	var unit = first_units[0]
-	while unit:
-		print("Unit Type:", unit.unit_type)
-		print("   Instruction Type:", unit.instr.type)
-		print("   Program Counter:", unit.instr.pc)
-		print("   Is Stalled:", unit.is_stalled)
-		print("")
-		unit = unit.next_unit
+	if not first_units.is_empty():
+		var unit = first_units[0]
+		while unit:
+			print("Unit Type:", unit.unit_type)
+			print("   Instruction Type:", unit.instr.type)
+			print("   Program Counter:", unit.instr.pc)
+			print("   Is Stalled:", unit.is_stalled)
+			print("")
+			unit = unit.next_unit
 
-	print("")
+		print("")
