87c77e75338d52efda21edf6b965de8de0346210
ppataky <patrick.pataky@epfl.ch>
2024-05-18 18:55:17 +0200

    The score is correctly displayed

--- ScorePanel.gd
+++ ScorePanel.gd
@@ -4,5 +4,6 @@ func _ready():
 	pass
 
 func set_label(score):
-	var label = get_node("Label")
-	label.text = "Score: " + str(score) + " IPC"
+	var label = get_child(0).get_child(0)
+		
+	label.text = "Score: " + str(snapped(score, 0.01)) + " IPC"
