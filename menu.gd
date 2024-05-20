224ab827ac3b428dff35be3abd8bb36da1b45311
Clément Josso <clement.josso@epfl.ch>
2024-05-20 09:51:56 +0200

    add game menu

---
+++ menu.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$VBoxContainer/Start.grab_focus()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_start_pressed():
+	get_tree().change_scene_to_file("res://introduction_1.tscn")
+
+
+func _on_exit_pressed():
+	get_tree().quit()
